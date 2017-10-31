c7e4c6bdae248d8c2beb92106a7a3577d9ba6f83
Artem Zakharov <zart208@gmail.com>
2017-10-31 23:33:19 +0300

    Commit with exercise_02 changes

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -2,42 +2,64 @@ package com.loftschool.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isEnabledAddButton = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        setTitle("Новая запись");
+        final EditText captionEdit = findViewById(R.id.caption);
+        final EditText coastEdit = findViewById(R.id.coast);
+        final ImageButton addButton = findViewById(R.id.add_button);
+        captionEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        System.out.println("onStart() override");
-    }
+            }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        System.out.println("onStop() override");
-    }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                isEnabledAddButton = (!TextUtils.isEmpty(charSequence) && coastEdit.getText().length() != 0);
+            }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        System.out.println("onDestroy() override");
-    }
+            @Override
+            public void afterTextChanged(Editable editable) {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        System.out.println("onPause() override");
-    }
+            }
+        });
+        coastEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        System.out.println("onResume() override");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                isEnabledAddButton = (!TextUtils.isEmpty(charSequence) && captionEdit.getText().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isEnabledAddButton) {
+                    Toast.makeText(MainActivity.this, "Necessary fields not filled", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
